207d8351c82014e713c7cb354f860c4c54cea09c
Christina Mary <chrisstinaa31@gmail.com>
2022-01-12 13:08:15 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { CssBaseline } from "@material-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline></CssBaseline>
+      <h1>Dictionary App </h1>
+    </div>
+  );
+}
+
+export default App;
